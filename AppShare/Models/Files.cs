63514ba7bec663f9dd9f2c4b9a7a02b1ef974ff3
shaijukumar <shaijukumar1980@gmail.com>
2018-11-26 05:22:15 +0400

    File module

---
+++ AppShare/Models/Files.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppShare.Models
+{
+    public class Files
+    {
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public string Description { get; set; }
+
+    }
+}
